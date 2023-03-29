4dd248dc67ec94533b7702509eed1fc1b60bbf46
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-03-29 20:34:02 +0300

    vercel backend init

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
+const middleware = jsonServer.defaults();
+
+server.use(middleware);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "$1",
+  })
+);
+server.use(router);
+const port = 3001;
+server.listen(port, () => {
+  console.log("server is listening on ", port);
+});
+
+module.exports = server;
